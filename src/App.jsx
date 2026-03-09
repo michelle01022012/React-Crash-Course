f6a055bf3dfb27cfe50d5e0ce1ec8d94c0053d36
Michelle Anderson <manderson0102@yahoo.com>
2026-03-08 22:51:05 -0400

    commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Todo from './components/Todo.jsx';
+import Title from './components/Title.jsx';
+import Modal from './components/Modal.jsx';
+
+
+function App() {
+  return (
+    <div>
+       <Title />
+       <div className="todo__wrapper">
+       <Todo title="Finish Frontend Simplified" />
+       <Todo title="Finish Interview Section" />
+       <Todo title="Land a 100k Job!" />
+     </div> 
+     <Modal />
+    </div>
+  );
+}  
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import './Todo.css'
+
+function Todo(props) {
+    return (
+    <div className="todo">
+      <h2>{ props.title }</h2>
+      <button>Delete</button>
+    </div>
+    );
+}
+
+export default Todo
