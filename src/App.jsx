b18c625b967a6acbb5bcba986f35d6732b789373
Michelle Anderson <manderson0102@yahoo.com>
2026-03-12 17:57:22 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import './App.css';
-import Todo from './components/Todo.jsx';
-import Title from './components/Title.jsx';
-import Modal from './components/Modal.jsx';
-
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx";
+import Modal from "./components/Modal.jsx";
 
 function App() {
   return (
     <div>
-
-       <Title />
-       <div className="todo__wrapper">
-       <Todo 
-          title="Finish Frontend Simplified" 
+      <Title />
+      <div className="todo__wrapper">
+        <Todo
+          title="Finish Frontend Simplified"
           paragraph="Code along with Frontend Simplified step by step."
-        />"
-       <Todo 
+        />
+        "
+        <Todo
           title="Finish Interview Section"
           paragraph="Finish every interview question in the next 6 weeks."
-      />
-
-       <Todo
-           title="Land a 100k Job!"
-           paragraph="Apply to 100 jobs."
-      />
-     </div> 
-     <Modal title="Are you sure you want to delete?"/>
-     </div>
+        />
+        <Todo title="Land a 100k Job!" paragraph="Apply to 100 jobs." />
+      </div>
+      <Modal title="Are you sure you want to delete?" />
+    </div>
   );
-}  
+}
 
 export default App;
