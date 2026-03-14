d14d1bb0bdadc85c3c18781f7568d1229872e1b3
Michelle Anderson <manderson0102@yahoo.com>
2026-03-14 12:10:08 -0400

    commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
 
 function Counter() {
-      const [counter, setCounter] = useState(0)
-      function incrementCounter() {
-        setCounter(counter + 1)
-      }
-   
-    function decrementCounter() {
-        setCounter(counter - 1)
+    const [cart, setCart] = useState({
+         item: "apple", 
+         quantity: 0
+    });
+
+function removeApple() {
+    // 1. Use a callback to get the previous value
+    // 2. Spread out all the properties of the prev state
+    // 3. Only change the property that you need to change
+        setCart(prevCart => ({
+            ...prevCart,
+            quantity: prevCart.quantity - 1,
+        }))
+    }    
+
+    function addApple() {
+        setCart(prevCart => ({
+            ...prevCart,
+            quantity: prevCart.quantity + 1,
+        }))
     }
     return (
         <div>
-            <button>-</button>
-             {0}
-            <button>+</button>
-        </div>
+            <button onClick={removeApple}>-</button>
+            {cart.quantity}
+            {cart.item}
+             <button onClick={addApple}>+</button>
+       </div>
     )
 }
 
-export default Counter 
+export default Counter;
