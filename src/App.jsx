1f8e00c161497909d5fb39a9fa3bb247926f30bd
Michelle Anderson <manderson0102@yahoo.com>
2026-03-14 20:33:53 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,29 @@
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Title from "./components/Title.jsx";
-import Modal from "./components/Modal.jsx";
-import Counter from "./components/Counter.jsx"
-import React, { useState, useEffect } from 'react';
+import { Browser as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
-
-  function onTodoDelete() {
-    setShowModal(true);
-   }
-
-  function cancelModal() {
-    setShowModal(false);
-  }
-
-  function confirmModal() {
-    setShowModal (false);
-  }
-
-   useEffect(() => {
-  }, [showModal])
-
-  useEffect(() => {
-    console.log('EVERY render')
-  })
-
+  
   return (
     <div>
-      <Title />
-      <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button onClick={()  => setShowModal(true)}>Add ToDo</button>
-      </div>
-      <div className="todo__wrapper">  
-        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified" />
-        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
-        <Todo onTodoDelete={onTodoDelete} title="Land a 100k Job!" />
-       </div>
-     {showModal && (
-      <Modal
-        cancelModal={cancelModal}
-        confirmModal={confirmModal}
-        title="Confirm Delete?"
-       />
-     )}
+      <Router>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/">about</a>
+          <a href="/">contact</a>
+        </nav>
+        <Routes>
+         <Route path="/" elements={<Home />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
-  );
-}
+    );
+  }
+     
 
 export default App;
