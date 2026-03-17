863142fe5249a24f3c7b3ce5a8916e15b47c7687
Michelle Anderson <manderson0102@yahoo.com>
2026-03-17 17:32:36 -0400

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,15 +2,28 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function Home() {
-    const [users, setUsers] = useState( {} )
-       async function fetchUsers() {
-       const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
-      }
+    const [users, setUsers] = useState([]);
+
+    async function fetchUsers() {
+        const { data } = await axios.get(
+         "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data); 
+}  
+    
     useEffect(() => {
-     fetchUsers();
+        setTimeout(() => {
+          fetchUsers();
 }, []);
 
-    return <h1>{users.length > 0 ? users[0].name : null}</h1>;
+    return (
+      <div>
+        {users.length > 0
+      ? <h1>{users[0]?.name}</h1>
+      :  <h1>Loading...</h1>
+    }
+    </div>
+);
 }
 
 export default Home
